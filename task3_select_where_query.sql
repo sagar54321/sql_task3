73ae157b44d4c97a24e58e99055f2005871a460d
Sagar Moon <moonsagar220@gmail.com>
2024-06-22 12:06:24 +0530

    sql_task3_completed

---
+++ task3_select_where_query.sql
@@ -0,0 +1,37 @@
+select * from gada_electronics_items
+
+select productname from gada_electronics_items
+
+select category from gada_electronics_items
+
+select productname , category , price from gada_electronics_items
+
+select distinct category from gada_electronics_items
+
+select distinct category, brand from gada_electronics_items
+
+select * from gada_electronics_items where brand = 'Realme'
+
+select * from gada_electronics_items where category = 'Laptops'
+
+select * from gada_electronics_items where stockquantity > 40
+
+select * from gada_electronics_items where stockquantity > 40 or price > 500
+
+select * from gada_electronics_items where stockquantity > 40 or category = 'Cameras'
+
+select * from gada_electronics_items where stockquantity > 40 AND category = 'Accessories'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
